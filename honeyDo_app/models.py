e071d1f7df4915970bf4c5d045348eb9502857f4
spiano <lafleurpiano@gmail.com>
2019-12-26 21:08:41 -0600

    created and migrated models

--- honeyDo_app/models.py
+++ honeyDo_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=180)
+    date = models.DateField(auto_now=False, auto_now_add=False, default=date.today)
+    person = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    task = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.task
