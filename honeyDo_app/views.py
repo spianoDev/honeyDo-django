632e62bfc4dd87f114bf4df281e27e554b5f0647
spiano <lafleurpiano@gmail.com>
2019-12-30 23:40:55 -0600

    added read views

--- honeyDo_app/views.py
+++ honeyDo_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Todo, Item
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todo_list.html', {'todos': todos})
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id = pk)
+    return render(request, 'todo_detail.html', {'todo': todo})
