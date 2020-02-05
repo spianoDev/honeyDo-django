73aaf2640b4a1c9f924f9af1be6dbbe09e79e69e
spiano <lafleurpiano@gmail.com>
2020-02-05 11:37:52 -0600

    changed path names to fix api vs views confusion

--- honeyDo_app/urls.py
+++ honeyDo_app/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     path('item/<int:pk>/delete', views.item_delete, name = 'item_delete'),
 # paths for the API portion of the deployed application
     path('api/', views.TodoList.as_view(), name = 'todo_list'),
-    path('api/todo/<int:pk>', views.TodoDetail.as_view(), name = 'todo_detail'),
-    path('api/todo/new', views.TodoDetail.as_view(), name = 'todo_create'),
-    path('api/item/new', views.ItemDetail.as_view(), name = 'item_create'),
-    path('api/item/<int:pk>/update', views.ItemDetail.as_view(), name = 'item_update'),
-    path('api/item/<int:pk>/delete', views.ItemDetail.as_view(), name = 'item_delete'),
-    path('api/todo/<int:pk>/delete', views.TodoDetail.as_view(), name = 'todo_delete'),
+    path('api-todo/<int:pk>', views.TodoDetail.as_view(), name = 'todo_detail'),
+    path('api-todo/new', views.TodoDetail.as_view(), name = 'todo_create'),
+    path('api-item/new', views.ItemDetail.as_view(), name = 'item_create'),
+    path('api-item/<int:pk>/update', views.ItemDetail.as_view(), name = 'item_update'),
+    path('api-item/<int:pk>/delete', views.ItemDetail.as_view(), name = 'item_delete'),
+    path('api-todo/<int:pk>/delete', views.TodoDetail.as_view(), name = 'todo_delete'),
 ]
