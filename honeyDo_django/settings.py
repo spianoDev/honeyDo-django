941e5491032c600b582ddfb6ec465fdd04cb0c45
spiano <lafleurpiano@gmail.com>
2020-02-04 22:23:32 -0600

    prep framework for heroku deployment

--- honeyDo_django/settings.py
+++ honeyDo_django/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'u$q(yte0r2n-66)o$$!fsxgnp(f_^#8s)xb)(v=77m!7kj-%y5'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
