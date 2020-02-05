a3cab3c6350eb8e921a9446dca9368ab57325c63
spiano <lafleurpiano@gmail.com>
2020-02-04 22:36:44 -0600

    added hyperlinks to rest framework

--- honeyDo_app/serializers.py
+++ honeyDo_app/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from .models import Todo, Item
 
-class TodoSerializer(serializers.ModelSerializer):
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
 
+    tasks = serializers.HyperlinkedRelatedField(
+        view_name = 'item_detail',
+        many = True,
+        read_only = True
+    )
+    todo_url = serializer.ModelSerializer.serializer_url_field(
+        view_name = 'todo_detail'
+    )
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'date', 'person',]
+        fields = ['id', 'title', 'date', 'person', 'todo_detail',]
 
 class ItemSerializer(serializers.ModelSerializer):
 
